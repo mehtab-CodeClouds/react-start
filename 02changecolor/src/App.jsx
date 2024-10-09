b4b2c26e13ad9bd5ccc536f4a86899ea5f1d6ef9
mehtab-CodeCLouds <mehtab.alam@codeclouds.com>
2024-10-09 09:58:05 +0530

    added new file bgchanger

--- 02changecolor/src/App.jsx
+++ 02changecolor/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "./App.css";
 import Button from "./components/Button.jsx";
 
 function App() {
--- 02changecolor/src/components/Button.jsx
+++ 02changecolor/src/components/Button.jsx
@@ -3,9 +3,18 @@ import "../App.css";
 
 function Button({ colorName = "white" }) {
   //   PaymentResponse.colorName = "red";
-  console.log(colorName);
+  //   console.log(colorName);
+  const [color, setColor] = useState("white");
+  //   console.log(color);
 
-  return <button className={`bg-${colorName}-500`}>{colorName}</button>;
+  return (
+    <button
+      className={`bg-${colorName}-500`}
+      onClick={() => setColor(colorName)}
+    >
+      {colorName}
+    </button>
+  );
 }
 
 export default Button;
